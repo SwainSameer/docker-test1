b4504b3adc85e59dd6a8a89910ffcd618e0ef1b4
sameer <sameerswain634@gmail.com>
2025-05-19 22:15:15 +0530

    demo-app-v1

---
+++ src/main/java/com/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class DemoController {
+	
+	@GetMapping("/welcome")
+	 public String getMsg() {
+		 return "wel come to Sameer It...!!";
+	 }
+
+}
